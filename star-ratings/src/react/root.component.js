367d700065b6d4be1f304e64a394ebe951f2bda9
Javier Llerenas <javr.llerenas@gmail.com>
2020-02-16 11:42:57 -0800

    Star Ratings: refactor initial state from props

--- star-ratings/src/react/root.component.js
+++ star-ratings/src/react/root.component.js
@@ -3,7 +3,7 @@ import StarRatings from "./components/StarRatings";
 import "../styles/app-container.css";
 
 const App = () => (
-  <div class="app-container">
+  <div className="app-container">
     <h1>React App</h1>
     <StarRatings />
   </div>
