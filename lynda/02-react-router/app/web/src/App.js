b51c3511226799a0b5e4a9098bc326f3a6190ae8
Javier Llerenas <llerej@amazon.com>
2020-03-03 13:34:58 -0800

    lynda(router): add files

--- lynda/02-react-router/app/web/src/App.js
+++ lynda/02-react-router/app/web/src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
-import Home from './components/Home';
-import Navigation from './components/Navigation';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import logo from "./logo.png";
+import "./App.css";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
+import Vitamin from "./components/Vitamin";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      toggleLogo: true,
-    }
+      toggleLogo: true
+    };
     this.toggleLogo = this.toggleLogo.bind(this);
     this.openNav = this.openNav.bind(this);
     this.closeNav = this.closeNav.bind(this);
   }
 
   toggleLogo(event) {
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       toggleLogo: !prevState.toggleLogo
     }));
   }
@@ -31,23 +33,37 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} 
-            className={this.state.toggleLogo ? 'static-logo' : 'static-logo animated jello'} 
-            alt="logo"
-            onMouseEnter={this.toggleLogo}
-            onMouseLeave={this.toggleLogo}
-            onClick={this.openNav}
-          />
-          <h1
-            className={this.state.toggleLogo ? 'menu-hidden' : 'menu animated bounceInDown'}
-            onClick={this.openNav}
-          >Menu</h1>
-          <Navigation closeNav={this.closeNav} />
-        </header>
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img
+              src={logo}
+              className={
+                this.state.toggleLogo
+                  ? "static-logo"
+                  : "static-logo animated jello"
+              }
+              alt="logo"
+              onMouseEnter={this.toggleLogo}
+              onMouseLeave={this.toggleLogo}
+              onClick={this.openNav}
+            />
+            <h1
+              className={
+                this.state.toggleLogo
+                  ? "menu-hidden"
+                  : "menu animated bounceInDown"
+              }
+              onClick={this.openNav}
+            >
+              Menu
+            </h1>
+            <Navigation closeNav={this.closeNav} />
+          </header>
+          
+          <Home />
+        </div>
+      </Router>
     );
   }
 }
