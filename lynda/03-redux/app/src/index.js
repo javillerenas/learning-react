16c75d10ce6da8477e5915f78d3b037cd0971cbe
Javier Llerenas <llerej@amazon.com>
2020-03-03 16:39:20 -0800

    lynda-redux(reducers + TDD): create reducers for the store + add TDD tests

--- lynda/03-redux/app/src/index.js
+++ lynda/03-redux/app/src/index.js
@@ -1,5 +1,6 @@
 import C from "./constants";
-import { goal } from "./store/reducers";
+import { goal, skyDay, errors } from "./store/reducers";
+import expect from "expect";
 
 /** Initial State */
 const state = 10;
@@ -8,16 +9,38 @@ const state = 10;
 const action = {
   type: C.SET_GOAL,
   payload: 15
-}
+};
 
 /** change the state through the action */
-const nextState = goal(state, action)
+const nextState = goal(state, action);
+expect(nextState).toEqual(action.payload);
 
-/** test */
-console.log(`
+// ====== SKY DAY ========
+const state2 = null;
+const action2 = {
+  type: C.ADD_DAY,
+  payload: {
+    resort: "Whistler",
+    date: "2020-15-01",
+    powder: true,
+    backcountry: false
+  }
+};
+const nextState2 = skyDay(state2, action2);
+expect(nextState2).toEqual(action2.payload);
 
-  init goal: ${state}
-  action ${JSON.stringify(action)}
-  new goal: ${nextState}
+// ====== ERRORS ========
+const errorState = ["random error"];
+const errorActionAdd = {
+  type: C.ADD_ERROR,
+  payload: "added by Action"
+};
+const errorNextState = errors(errorState, errorActionAdd);
+expect(errorNextState).toEqual([...errorState, errorActionAdd.payload]);
 
-`)
+const errorActionClear = {
+  type: C.CLEAR_ERROR,
+  payload: "added by Action"
+};
+const errorNextState2 = errors(errorState, errorActionClear);
+expect(errorNextState2).toEqual(errorState);
--- lynda/03-redux/app/src/store/reducers.js
+++ lynda/03-redux/app/src/store/reducers.js
@@ -1,14 +1,78 @@
 import C from "../constants";
 
 /**
- * 
+ *
  * @param {*} state Number: represents the goal
  * @param {*} action { type: String, payload: Number }
  */
-export const goal = (state=10, action) => {
-  if (action.type === C.SET_GOAL) {
-    return parseInt(action.payload)
-  } else {
-    return state
+export const goal = (state = 10, action) =>
+  action.type === C.SET_GOAL ? parseInt(action.payload) : state;
+
+/**
+ *
+ * @param {*} state
+ * @param {*} action { type: String, payload: {}}
+ */
+export const skyDay = (state = null, action) =>
+  action.type === C.ADD_DAY ? action.payload : state;
+
+/**
+ *
+ * @param {*} state
+ * @param {*} action
+ */
+export const errors = (state = [], action) => {
+  switch (action.type) {
+    case C.ADD_ERROR:
+      return [...state, action.payload];
+
+    case C.CLEAR_ERROR:
+      return state.length > 0 ? removeFromArray(state, action.payload) : state;
+
+    default:
+      return state;
   }
+};
+
+function removeFromArray(array, toRemove) {
+  return array.filter(item => item !== toRemove);
 }
+
+export const allSkyDays = (state = [], action) => {
+  switch (action.type) {
+    case C.ADD_DAY:
+      return state.find(action.payload) ? state : [...state, action.payload];
+
+    case C.REMOVE_DAY:
+      return state.filter(day => day !== action.payload);
+  }
+};
+
+export const fetching = (state = false, action) => {
+  switch (action.type) {
+    case C.FETCH_RESORT_NAMES:
+      return true;
+
+    case C.CANCEL_FETCHING:
+      return false;
+
+    case C.CHANGE_SUGGESTIONS:
+      return false;
+
+    default:
+      return state;
+  }
+};
+
+export const suggestions = (state = [], action) => {
+  switch (action.type) {
+    case C.CLEAR_SUGGESTIONS:
+      return [];
+
+    case C.CHANGE_SUGGESTIONS:
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
